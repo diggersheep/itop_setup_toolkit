7f88bdb631e34bd8bfa3197f4d106be10c61b470
Alexandre COMBEAU <alexandre.combeau@outlook.fr>
2022-03-23 22:33:13 +0100

    Update apply-setup.sh

--- apply-setup.sh
+++ apply-setup.sh
@@ -1,4 +1,4 @@
-php /tmp/generate-default-params.php /tmp/default-params.php.template /var/www/html/toolkit/default-params.xml $1
+php /tmp/generate-default-params.php /tmp/default-params.php.template /var/www/html/toolkit/default-params.xml $1 $2
 
 cd /var/www/html/toolkit/
 php /var/www/html/toolkit/setup.php
